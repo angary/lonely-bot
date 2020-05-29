d427750b289fa375d13abd3614a844c6c946dd2c
Gary Sun <garysun0451@gmail.com>
2020-05-30 01:52:12 +1000

    Added maximum possible pentagon area function
    Sorts the original arguments numerically into ascending order, and then places these values into a new array, which is also sorted into an order which will produce the largest possible pentagonal area.

--- commands/pentagon.js
+++ commands/pentagon.js
@@ -5,21 +5,31 @@ module.exports = {
     usage: [`[Fighting] [Farming] [Supporting] [Pushing] [Versatility]`],
 	execute(message, args) {
 
-        // If there were not enough arguments
         if (args.length != 5) {
             return message.channel.send(`You didn't give 5 values`);
         }
-        let area = 0;
-        for (let i = 0; i < 4; i++) {
-            if (0 <= args[i] && args[i] <= 10) {
-                area += (0.5) * (args[i]) * (args[i + 1]) * (Math.sin(72 * Math.PI / 180));
+
+        function area(lengths) {
+            let sum = 0;
+            for (let i = 0; i < 4; i++) {
+                if (0 <= lengths[i] && lengths[i] <= 10) {
+                    sum += (0.5) * (lengths[i]) * (lengths[i + 1]) * (Math.sin(72 * Math.PI / 180));
+                }
+                else {
+                    return message.channel.send(`${lengths[i]} was not a valid value`);
+                }
             }
-            else {
-                return message.channel.send(`${args[i]} was not a valid value`);
-            }
+            sum += (0.5) * (lengths[0]) * (lengths[4]) * (Math.sin(72 * Math.PI / 180));
+
+            return sum;
         }
-        area += (0.5) * (args[0]) * (args[4]) * (Math.sin(72 * Math.PI / 180));
         
-        return message.channel.send(`The area of your pentagon is ${area}!`);
+        args.sort((a,b)=>a-b);
+        let sortedArgs = [args[1], args[3], args[4], args[2], args[0]];
+        let proportion = (area(args) * 100) / area(sortedArgs);
+
+        message.channel.send(`The area of your pentagon is **${area(args)}**`);
+        message.channel.send(`Max area of your pentagon is **${area(sortedArgs)}**`);
+        message.channel.send(`Your pentagon is **${proportion.toFixed(2)}%** of it's maximum area`)
 	},
 };
