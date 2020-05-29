a96e68ff3f11ce119370236f10106d9c0be981cd
Gary Sun <garysun0451@gmail.com>
2020-05-30 02:18:17 +1000

    Added array to keep original values of the arguments

--- commands/pentagon.js
+++ commands/pentagon.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'pentagon',
-    aliases: [`areapentagon`, `dotapentagon`],
-    description: `Given 5 values of the player's pentagon, it gives the area of the pentagon`,
+    aliases: [`areapentagon`, `dotapentagon`, `p`],
+    description: `Given 5 values of the player's pentagon, it gives the area of the pentagon with those values, the maximum possible area by swapping positions of those values, and the ratio fo area of the given area to the maximum area`,
     usage: [`[Fighting] [Farming] [Supporting] [Pushing] [Versatility]`],
 	execute(message, args) {
 
@@ -20,15 +20,17 @@ module.exports = {
                 }
             }
             sum += (0.5) * (lengths[0]) * (lengths[4]) * (Math.sin(72 * Math.PI / 180));
-
             return sum;
         }
-        
+
+        let unsortedArgs = [args[0], args[1], args[2], args[3], args[4]];
         args.sort((a,b)=>a-b);
         let sortedArgs = [args[1], args[3], args[4], args[2], args[0]];
-        let proportion = (area(args) * 100) / area(sortedArgs);
+        let proportion = (area(unsortedArgs) * 100) / area(sortedArgs);
 
-        message.channel.send(`The area of your pentagon is **${area(args)}**`);
+
+        message.channel.send(`running on gary's pc`)
+        message.channel.send(`The area of your pentagon is **${area(unsortedArgs)}**`);
         message.channel.send(`Max area of your pentagon is **${area(sortedArgs)}**`);
         message.channel.send(`Your pentagon is **${proportion.toFixed(2)}%** of it's maximum area`)
 	},
