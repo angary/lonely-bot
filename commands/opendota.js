07a49729c8d5cf9dcf9d907fdff311819572a2f2
Gary Sun <garysun0451@gmail.com>
2020-05-30 15:25:17 +1000

    better scope implentation of variables, dictionary for medals, and better message formatting

--- commands/opendota.js
+++ commands/opendota.js
@@ -1,3 +1,4 @@
+// My code's a bit scuffed in this file. Still figuring things out
 const fetch = require('node-fetch');
 
 module.exports = {
@@ -7,18 +8,42 @@ module.exports = {
     args: true,
     usage: `<steamID>`,
     cooldown: 5,
-    
     async execute(message, args) {
 
-        global.name = "string";
-        global.picture = "string";
-        global.country = "string";
-        global.rank_tier = "0";
-        global.mmr_estimate = "0";
-        global.won = "0";
-        global.lost = "0";
+        // Declared variables
+        let name = "string";
+        let picture = "string";
+        let country = "string";
+        let rank_tier = "0";
+        let mmr_estimate = "0";
+        let won = "0";
+        let lost = "0";
 
+        // URL for fetching API
         let url = `https://api.opendota.com/api/players/${args[0]}`
+
+        // TODO: Implement method for immortal
+        function medal(rank) {
+
+            if (rank === "none") {
+                return "unranked";
+            }
+
+            let medal_tier = rank.toString();
+            
+            let medals = {
+                "1": "Herald",
+                "2": "Guardian",
+                "3": "Crusader",
+                "4": "Archon",
+                "5": "Legend",
+                "6": "Ancient",
+                "7": "Divine"
+            }
+            
+            return `${medals[medal_tier[0]]} ${medal_tier[1]}`;
+        }
+
 
         try {
             // Finds general information on the player
@@ -31,16 +56,17 @@ module.exports = {
                         }
                         response.json().then(function (data) {
                             console.log(data);
-                            global.name = data.profile.personaname;
-                            global.picture = data.profile.avatar;
-                            global.country = data.profile.loccountrycode;
-                            global.rank_tier = data.rank_tier;
-                            global.mmr_estimate = data.mmr_estimate.estimate;
+                            name = data.profile.personaname;
+                            picture = data.profile.avatar;
+                            country = data.profile.loccountrycode;
+                            rank_tier = data.rank_tier;
+                            mmr_estimate = data.mmr_estimate.estimate;
                         });
                     }
                 )
                 .catch(function (err) {
-                    message.channel.send('Fetch Error :-S', err);
+                    message.channel.send('Fetch Error :-S');
+                    console.log(err);
                 });
             
             // Finds information on player win loss
@@ -53,51 +79,50 @@ module.exports = {
                         }
                         response.json().then(function (data) {
                             console.log(data);
-                            global.won = data.win;
-                            global.lost = data.lose;
-                            message.channel.send(`**name:** ${name} **country:** ${country} **medal:** ${rank_tier} **mmr:** ${mmr_estimate} **win:** ${won} **lose:** ${lost} `)
+                            won = data.win;
+                            lost = data.lose;
+
+                            // Temporary, as I can't get callback to work, and embed is not workinf=g
+                            message.channel.send(`Steam name: **${name}** \nCountry: **${country}** \nMedal: **${medal(rank_tier)}** \nEstimated MMR: **${mmr_estimate}** \nWon games: **${won}** \nLost games: **${lost}** \nWinrate: **${(100 * won/(won + lost)).toFixed(2)}%**`)
                         });
                     }
                 )
                 .catch(function (err) {
-                    message.channel.send('Fetch Error :-S', err);
+                    message.channel.send('Fetch Error :-S');
+                    console.log(err);
                 });
-            
-
         }
         catch (error) {
             message.channel.send(`error :-S`, error);
         }
-
-        
-        message.channel.send(`If this message shows up first before anything else, there's an async problem, also I can't make an embed? So formatting is cucked`);
-
-        //     {
-        //         "tracked_until": "string",
-        //         "solo_competitive_rank": "string",
-        //         "competitive_rank": "string",
-        //         "rank_tier": 0,
-        //         "leaderboard_rank": 0,
-        //         "mmr_estimate": {
-        //             "estimate": 0,
-        //             "stdDev": 0,
-        //             "n": 0
-        //         },
-        //         "profile": {
-        //             "account_id": 0,
-        //             "personaname": "string",
-        //             "name": "string",
-        //             "plus": true,
-        //             "cheese": 0,
-        //             "steamid": "string",
-        //             "avatar": "string",
-        //             "avatarmedium": "string",
-        //             "avatarfull": "string",
-        //             "profileurl": "string",
-        //             "last_login": "string",
-        //             "loccountrycode": "string",
-        //             "is_contributor": false
-        //         }
-        //     }
 	},
 };
+
+// message.channel.send(`If this message shows up first before anything else, there's an async problem, also I can't make an embed? So formatting is cucked`);
+// {
+//     "tracked_until": "string",
+//     "solo_competitive_rank": "string",
+//     "competitive_rank": "string",
+//     "rank_tier": 0,
+//     "leaderboard_rank": 0,
+//     "mmr_estimate": {
+//         "estimate": 0,
+//         "stdDev": 0,
+//         "n": 0
+//     },
+//     "profile": {
+//         "account_id": 0,
+//         "personaname": "string",
+//         "name": "string",
+//         "plus": true,
+//         "cheese": 0,
+//         "steamid": "string",
+//         "avatar": "string",
+//         "avatarmedium": "string",
+//         "avatarfull": "string",
+//         "profileurl": "string",
+//         "last_login": "string",
+//         "loccountrycode": "string",
+//         "is_contributor": false
+//     }
+// }
