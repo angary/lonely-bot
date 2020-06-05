c6fa2616f8d2d78f07b05d1e7bc6df2936e574a1
Gary Sun <garysun0451@gmail.com>
2020-06-05 12:49:28 +1000

    Added percentile data, and formatted embed

--- commands/opendota.js
+++ commands/opendota.js
@@ -8,9 +8,9 @@ module.exports = {
     args: true,
     usage: `<steamID>`,
     cooldown: 1,
-    async execute(message, args) {
+    execute(message, args) {
 
-        // Declared variables
+        // Embed information
         let p = {
             name: "don't",
             pic: "asdf",
@@ -20,11 +20,13 @@ module.exports = {
             won: "0",
             lost: "0",
             hero: [
-                {"hero_id":"0","last_played":0,"games":0,"win":0,"with_games":0,"with_win":0,"against_games":0,"against_win":0},
-                {"hero_id":"0","last_played":0,"games":0,"win":0,"with_games":0,"with_win":0,"against_games":0,"against_win":0},
-                {"hero_id":"0","last_played":0,"games":0,"win":0,"with_games":0,"with_win":0,"against_games":0,"against_win":0}
+                {"hero_id":"0","last_played":0,"games":0,"win":0,"with_games":0,"with_win":0,"against_games":0,"against_win":0,"percentile":0},
+                {"hero_id":"0","last_played":0,"games":0,"win":0,"with_games":0,"with_win":0,"against_games":0,"against_win":0,"percentile":0},
+                {"hero_id":"0","last_played":0,"games":0,"win":0,"with_games":0,"with_win":0,"against_games":0,"against_win":0,"percentile":0}
             ]
         }
+
+        let time_recieved = Date.now();
 
         // TODO: Implement method for immortal
         function medal(rank) {
@@ -44,14 +46,19 @@ module.exports = {
             return `${medals[medal_tier[0]]} ${medal_tier[1]}`;
         }
 
-        let base_url = `https://api.opendota.com/api/players/${args[0]}`
+        let base_url = `https://api.opendota.com/api/players/${args[0]}`;
         let profile = fetch (base_url);
         let win_lose = fetch (`${base_url}/wl`);
         let player_heroes = fetch(`${base_url}/heroes`);
         let heroes = fetch(`https://api.opendota.com/api/heroes`);
+        let player_rankings = fetch(`${base_url}/rankings`);
 
-        Promise.all([profile, win_lose, player_heroes, heroes])
+        Promise.all([profile, win_lose, player_heroes, heroes, player_rankings])
+
+            // Convert data to .json
             .then(response => Promise.all(response.map(response => response.json())))
+
+            // Extract data
             .then(data => {
 
                 // Profile details
@@ -60,132 +67,82 @@ module.exports = {
                 p.country = data[0].profile.loccountrycode;
                 p.rank_tier = data[0].rank_tier;
                 p.mmr_estimate = data[0].mmr_estimate.estimate;
-                // message.channel.send(`Steam name: **${p.name}** \nCountry: **${p.country}** \nMedal: **${medal(p.rank_tier)}** \nEstimated MMR: **${p.mmr_estimate}**`);
 
                 // Won and lost games
                 p.won = data[1].win;
                 p.lost = data[1].lose;
-                // message.channel.send(`Won games: **${p.won}** \nLost games: **${p.lost}** \nWinrate: **${(100 * p.won/(p.won + p.lost)).toPrecision(2)}%**`);
 
                 // Heroes
                 for (let i = 0; i < 3; i++) {
                     p.hero[i] = data[2][i];
+                    for (let j = 0; j < data[4].length - 1; j++) {
+                            if (data[4][j].hero_id == data[2][i].hero_id) {
+                            p.hero[i].percentile = +(100 * data[4][j].percent_rank).toFixed(2);
+                            break;
+                        }
+                    }
                     for (let j = 0; i < data[3].length - 1; j++) {
                         if (data[3][j].id == data[2][i].hero_id) {
-                            p.hero[i].id = data[3][j].localized_name;
+                            p.hero[i].name = data[3][j].localized_name;
                             break;
                         } 
                     }
                 } 
-                // message.channel.send(`Best heroes: **${p.hero[0].id}**,  **${p.hero[1].id}**,  **${p.hero[2].id}**`);
+                // console.log(p);
             })
+
+            // Format data onto embed
             .then(() => {
                 const profileEmbed = new Discord.MessageEmbed()
                     .setColor('#0099ff')
                     .setTitle(`${p.name}`)
                     .setURL(`https://www.opendota.com/players/${args[0]}`)
-                    .setAuthor(`Lonely Bot`, 'https://i.imgur.com/wSTFkRM.png', 'https://github.com/Gy74S/Lonely-Bot')
+                    .setAuthor(`Lonely Bot`, 'https://cdn.discordapp.com/avatars/647044127313362980/9ca1222828d05412825fce12222ea48e.png?size=256', 'https://github.com/Gy74S/Lonely-Bot')
                     .setDescription(`Medal: **${medal(p.rank_tier)}**\nMMR Estimate: **${p.mmr_estimate}**\nCountry: **${p.country}**`)
                     .setThumbnail(p.pic)
                     .addFields(
                         {
                             name: '**Match data**', 
-                            value: `Total: **${p.won + p.lost}** | Won: **${p.won}** | Lost: **${p.lost}** | Winrate: **${(100 * p.won/(p.won + p.lost)).toPrecision(2)}%**`
-                        },
+                            value: `Total: **${p.won + p.lost}** | Won: **${p.won}** | Lost: **${p.lost}** | Winrate: **${(100 * p.won/(p.won + p.lost)).toPrecision(2)}%**\n`
+                        }
+                    )
+                    .addFields(
                         { 
-                            name: `**${p.hero[0].id}**`, 
+                            name: `**${p.hero[0].name}**`, 
                             value: `Games: **${p.hero[0].games}**
                                 Win as: **${(100*p.hero[0].win/p.hero[0].games).toPrecision(2)}%**
                                 Win with: **${(100*p.hero[0].with_win/p.hero[0].with_games).toPrecision(2)}%**
-                                Win against: **${(100*p.hero[0].against_win/p.hero[0].against_games).toPrecision(2)}%**`, 
+                                Win against: **${(100*p.hero[0].against_win/p.hero[0].against_games).toPrecision(2)}%**
+                                Percentile: **${p.hero[0].percentile}**`, 
                             inline: true 
                         },
                         { 
-                            name: `**${p.hero[1].id}**`, 
+                            name: `**${p.hero[1].name}**`, 
                             value: `Games: **${p.hero[1].games}**
                                 Win as: **${(100*p.hero[1].win/p.hero[1].games).toPrecision(2)}%**
                                 Win with: **${(100*p.hero[1].with_win/p.hero[1].with_games).toPrecision(2)}%**
-                                Win against: **${(100*p.hero[1].against_win/p.hero[1].against_games).toPrecision(2)}%**`,
+                                Win against: **${(100*p.hero[1].against_win/p.hero[1].against_games).toPrecision(2)}%**
+                                Percentile: **${p.hero[1].percentile}**`,
                             inline: true 
                         },
                         { 
-                            name: `**${p.hero[2].id}**`, 
+                            name: `**${p.hero[2].name}**`, 
                             value: `Games: **${p.hero[2].games}**
                                 Win as: **${(100*p.hero[2].win/p.hero[2].games).toPrecision(2)}%**
                                 Win with: **${(100*p.hero[2].with_win/p.hero[2].with_games).toPrecision(2)}%**
-                                Win against: **${(100*p.hero[2].against_win/p.hero[2].against_games).toPrecision(2)}%**`,
+                                Win against: **${(100*p.hero[2].against_win/p.hero[2].against_games).toPrecision(2)}%**
+                                Percentile: **${p.hero[2].percentile}**`,
                             inline: true 
                         }
                     )
-                    // .setImage(p.pic)
                     .setTimestamp()
-                    .setFooter(`I'll implement hero rating later, trust | Time taken: ${Date.now() - message.createdTimestamp}ms`); // Can take additional argument of a small picture
+                    .setFooter(`Total Processing Time: ${Date.now() - message.createdTimestamp} ms | Generating Time: ${Date.now() - time_recieved} ms`); // Can take additional argument of a small picture
 
                 message.channel.send(profileEmbed);
             })
             .catch(function(error) {
                 console.log(error);
+                message.channel.send(`There was an error: ${error}`);
             })
-            // responses[0].then(async function profile (data) {
-            //     console.log(data);
-            //     p.name = await data.profile.personaname;
-            //     p.picture = await data.profile.avatar;
-            //     p.country = await data.profile.loccountrycode;
-            //     p.rank_tier = await data.rank_tier;
-            //     p.mmr_estimate = await data.mmr_estimate.estimate;
-            //     // message.channel.send(`Steam name: **${p.name}** \nCountry: **${p.country}** \nMedal: **${medal(p.rank_tier)}** \nEstimated MMR: **${p.mmr_estimate}**`);
-            // })
-            // responses[1].then(async function win_lose (data) {
-            //     console.log(data);
-            //     p.won = await data.win;
-            //     p.lost = await data.lose;
-            //     // message.channel.send(`Won games: **${p.won}** \nLost games: **${p.lost}** \nWinrate: **${(100 * p.won/(p.won + p.lost)).toPrecision(2)}%**`);
-            // })
-            // .then (() => {
-            // })
-            // .catch(function (err) {
-            //     message.channel.send(`Fetch error ${err}`);
-            // })
 	},
 };
-
-// message.channel.send(`If this message shows up first before anything else, there's an async problem, also I can't make an embed? So formatting is cucked`);
-// {
-//     "tracked_until": "string",
-//     "solo_competitive_rank": "string",
-//     "competitive_rank": "string",
-//     "rank_tier": 0,
-//     "leaderboard_rank": 0,
-//     "mmr_estimate": {
-//         "estimate": 0,
-//         "stdDev": 0,
-//         "n": 0
-//     },
-//     "profile": {
-//         "account_id": 0,
-//         "personaname": "string",
-//         "name": "string",
-//         "plus": true,
-//         "cheese": 0,
-//         "steamid": "string",
-//         "avatar": "string",
-//         "avatarmedium": "string",
-//         "avatarfull": "string",
-//         "profileurl": "string",
-//         "last_login": "string",
-//         "loccountrycode": "string",
-//         "is_contributor": false
-//     }
-// }
-// [
-//     {
-//       "hero_id": "string",
-//       "last_played": 0,
-//       "games": 0,
-//       "win": 0,
-//       "with_games": 0,
-//       "with_win": 0,
-//       "against_games": 0,
-//       "against_win": 0
-//     }
-//   ]
