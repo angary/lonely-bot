87161fe3f710b0ddcd508594c784abf5eb9435f1
Gy74S <garysun0451@gmail.com>
2020-11-18 22:48:06 +1100

    Added steam, discord ID schema and ability to add a new document to add IDs to db
    However, it will add multiple documents rather than updating previous ones if they have the same discord id

--- commands/id.js
+++ commands/id.js
@@ -1,13 +1,24 @@
-const mongoose = require('mongoose');
+let User = require('../models/user')
 
 module.exports = {
-    name: 'id',
+    name: '>id',
     description: 'Link your current Discord ID to your Steam ID',
     aliases: false,
-    args: false,
+    args: true,
     usage: "[Steam32 ID]",
     cooldown: 1,
     execute(message, args) {
-        message.channel.send('Still implementing atm.')
+
+        // If the user has already set their IDs
+
+
+        // If the usre hasn't set their IDs
+        const discordID = message.author.id;
+        const steamID = args[0]
+
+        const newUser = new User({discordID, steamID});
+        newUser.save()
+            .then(() => message.channel.send(`Mapped Discord ID **${discordID}** to  Steam ID **${steamID}**`))
+            .catch(err => message.channel.send('Error: ' + err))
     },
 };
---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	discordID: { type: String, required: true },
+	steamID: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
