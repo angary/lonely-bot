2753dec3d6cf65416deef4d0f0f41dc85bfe24bd
Gy74S <garysun0451@gmail.com>
2020-11-23 18:45:41 +1100

    Added embed for help, and now sends information in the channel the message is in
    Also added a new field called information which contains more data about a command, whereas description contains a short one line information

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,5 @@
 const { prefix } = require('../config.json');
+const Discord = require('discord.js');
 
 module.exports = {
   name: 'help',
@@ -6,50 +7,53 @@ module.exports = {
   aliases: ['commands'],
   args: false,
   usage: '[command name]',
-  cooldown: 5,
+  cooldown: 0,
   execute (message, args) {
+
     const data = [];
     const { commands } = message.client;
-
+    
     // If they didn't specify a specific commmand
     if (!args.length) {
-      data.push('Here\'s a list of all my commands:');
 
-      // Makes it into a code block
-      data.push('```');
-      data.push(commands.map(command => command.name).join('\n'));
-      data.push('```');
-
-      data.push(`You can send \`${prefix}help [command name]\` to get info on a specific command!`);
-
-      return message.author.send(data, { split: true })
-        .then(() => {
-          if (message.channel.type == 'dm') return;
-          message.reply('I\'ve sent you a DM with all my commands!');
-        })
-        .catch(error => {
-          console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-          message.reply('it seems like I can\'t DM you!');
-        });
+      // Add all the details of the commands
+      data.push(
+        commands.map(command => `**${command.name}**: ${command.description}`).join('\n')
+      );
+      
+      // Format data and send am embed to channel
+      const helpEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Avaliable commands')
+        .setAuthor(
+          'Lonely Bot',
+          'https://i.imgur.com/b0sTfNL.png',
+          'https://github.com/Gy74S/Lonely-Bot'
+        )
+        .setDescription(data)
+        .setFooter(`You can send \`${prefix}help [command name]\` to get info on a specific command!`);
+      return message.channel.send(helpEmbed);
     }
 
-    // Else send them more specific information regarding how to use the command
+    // Else find information about the single command
     const name = args[0].toLowerCase();
     const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-
+    
     // If it cannot find a proper commmand name then respond
-    if (!command) {
-      return message.reply('that\'s not a valid command!');
-    }
-
-    data.push(`**Name:** ${command.name}`);
-
+    if (!command) return message.reply("that's not a valid command!");
+    
+    // Else collect relevant information and send
     if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-    if (command.description) data.push(`**Description:** ${command.description}`);
-    if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+    if (command.information) data.push(`**Information:** ${command.information}`);
+    else if (command.description) data.push(`**Information:** ${command.description}`);
+    if (command.usage) data.push(`**Usage:** \`${prefix}${command.name} ${command.usage}\``);
+    data.push(`**Cooldown:** ${command.cooldown} second(s)`);
+    
+    const helpEmbed = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle(`Help for => ${command.name}`)
+      .setDescription(data);
 
-    data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-
-    message.channel.send(data, { split: true });
+    message.channel.send(helpEmbed);
   }
 };
