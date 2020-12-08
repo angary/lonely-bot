a9e251b43b98250827b3873d43bdd68aa281b876
Gy74S <garysun0451@gmail.com>
2020-12-08 19:05:28 +1100

    Added ability to check for countesr for multiple heroes
    Also updated information in for help command

--- commands/counter.js
+++ commands/counter.js
@@ -2,11 +2,24 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const Discord = require('discord.js');
 const aliasToHeroName = require('../assets/heroNames');
+const information = `
+Given the enemy hero names seperated by commas, return the top counters by winrate and advantage.
+
+Explanation of data:
+
+**Winrate**
+- The numbers suggest the enemy's average winrate against the hero
+- Sorting by winrate suggests heroes that are in the meta
+
+**Disadvantage**
+- The numbers suggest the enemy's average disadvantage against the hero
+- Sorting by disadvantage suggests heroes that generally counter the heros based on their abilities, but may not be the best in the current meta
+`;
 
 module.exports = {
   name: 'counter',
   description: 'Returns a list of top counters to given heroes',
-  information: 'Given the enemy hero names seperated by commas, return the top 10 ideal and unideal picks for your team.',
+  information: information,
   aliases: false,
   args: true,
   usage: '[enemy_1], [enemy_2] ...',
@@ -34,10 +47,6 @@ async function counter (message, args) {
       return sendMessage(message.channel, `Invalid hero name **${name}**.`);
     }
   }
-  console.log(enemies);
-
-  // Object to store data for each hero
-  const heroes = {};
 
   // Webscrape the data
   const promises = enemies.map(hero => {
@@ -48,39 +57,12 @@ async function counter (message, args) {
   axios.all(promises)
 
     // Collect via webscraping
-    .then(async responses => {
-      for (const response of responses) {
-        // Grab the data from the counters table
-        const $ = await cheerio.load(response.data);
-        const counterTable = $(tableSelector);
-
-        // Extract data from each hero
-        counterTable.each((index, element) => {
-          // Data is in given in the form of a string
-          const name = $(element).find('td.cell-xlarge').text();
-          const disadvantage = $(element).find('td:nth-child(3)').text();
-          const winrate = $(element).find('td:nth-child(4)').text();
-
-          // Convert data into numbers and store them
-          heroes[name] = {};
-          heroes[name].name = name;
-          heroes[name].disadvantage = parseFloat(disadvantage.slice(0, -1));
-          heroes[name].winrate = parseFloat(winrate.slice(0, -1));
-        });
-      }
-    })
+    .then(async responses => aggregateData(responses, enemies))
 
     // Format data and send it
-    .then(() => {
-      // Convert data into an array
-      const heroesArray = Object.values(heroes);
+    .then((counters) => sendEmbed(message, timeRecieved, enemies, counters))
 
-      // Remove heroes if they are in the enemy team
-      const counters = heroesArray.filter(hero => !enemies.includes(hero.name));
-
-      // Sort counters based on disadvantage
-      sendEmbed(message, timeRecieved, enemies, counters);
-    })
+    // Handle errors
     .catch(error => {
       sendMessage(message.channel, `There was an error: ${error}`);
     });
@@ -89,6 +71,50 @@ async function counter (message, args) {
 // Find the number of allies and heroes in the argument
 function parseArgs (args) {
   return args.join('').split(',');
+}
+
+// Collect all relevant data from webscraping
+async function aggregateData (responses, enemies) {
+  const heroes = {};
+  // Extra data from each hero coutner request
+  for (const response of responses) {
+    // Grab the data from the counters table
+    const $ = await cheerio.load(response.data);
+    const counterTable = $(tableSelector);
+
+    // Extract data from each hero
+    counterTable.each((index, element) => {
+      // Data is in given in the form of a string
+      const name = $(element).find('td.cell-xlarge').text();
+      const disadvantage = $(element).find('td:nth-child(3)').text();
+      const winrate = $(element).find('td:nth-child(4)').text();
+
+      // Convert data into numbers and store them
+      if (name in heroes) {
+        // If hero exists in object, then increase stats
+        heroes[name].disadvantage += parseFloat(disadvantage.slice(0, -1));
+        heroes[name].winrate += parseFloat(winrate.slice(0, -1));
+        heroes[name].count += 1;
+      } else {
+        // Else if hero doesn't exist in object, initialise data
+        heroes[name] = {};
+        heroes[name].name = name;
+        heroes[name].disadvantage = parseFloat(disadvantage.slice(0, -1));
+        heroes[name].winrate = parseFloat(winrate.slice(0, -1));
+        heroes[name].count = 1;
+      }
+    });
+  }
+
+  // Find the average winrate and disadvantage
+  for (const key in heroes) {
+    const count = heroes[key].count;
+    heroes[key].winrate /= count;
+    heroes[key].disadvantage /= count;
+  };
+
+  // Convert object into array and remove heroes if they are in enemmy team
+  return Object.values(heroes).filter(hero => !enemies.includes(hero.name));
 }
 
 // Format data and send an embed to channel with details
@@ -110,24 +136,28 @@ function sendEmbed (message, timeRecieved, enemies, counters) {
   // Description formatting
   heroesEmbed.setDescription(`Heroes good against **${enemies.join(', ')}**`);
 
-  // Ideal pick formatting
-  addHeroes(heroesEmbed, counters, 'winrate', 5);
-  addHeroes(heroesEmbed, counters, 'disadvantage', 5);
-  // addHeroes(heroesEmbed, counters, 'disadvantage', 5);
+  // Add heroes with good winrates against enemy
+  const winCounters = counters.sort((a, b) => a.winrate - b.winrate).slice(0, 5);
+  addHeroes(heroesEmbed, winCounters, 'winrate', 5);
+
+  // Add heroes with good advantage against enemy
+  const advCounters = counters.sort((a, b) => b.disadvantage - a.disadvantage).slice(0, 5);
+  addHeroes(heroesEmbed, advCounters, 'disadvantage', 5);
+
   sendMessage(message.channel, heroesEmbed);
 }
 
 // Add fields to embed, given the hero details
-function addHeroes (heroesEmbed, counters, sortMethod, displayCount) {
-  // Sort the heroes
-  let winCounters = [...counters].sort((a, b) => a[sortMethod] - b[sortMethod]);
-  winCounters = winCounters.slice(0, 5);
-
+function addHeroes (heroesEmbed, counters, sortMethod) {
   // Add details to embed
   const details = [];
-  details[`Sorted by ${sortMethod}\n**Heroes**:`] = `**${winCounters.map(counter => counter.name).join('**\n**')}**`;
-  details['**\nDisadvantage**:'] = `${winCounters.map(counter => counter.disadvantage).join('%\n')}%`;
-  details['**\nWinrate**:'] = `${winCounters.map(counter => counter.winrate).join('%\n')}%`;
+  let heroes = '';
+  counters.forEach((element, index) => {
+    heroes += `${index + 1}: **${element.name}**\n`;
+  });
+  details[`**__Sorted by ${sortMethod}__\nHeroes**:`] = heroes;
+  details['**\nDisadvantage**:'] = `${counters.map(counter => counter.disadvantage.toFixed(2)).join('%\n')}%`;
+  details['**\nWinrate**:'] = `${counters.map(counter => counter.winrate.toFixed(2)).join('%\n')}%`;
   for (const [key, value] of Object.entries(details)) {
     heroesEmbed.addFields({
       name: key,
