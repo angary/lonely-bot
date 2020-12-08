37770123c0c426edc388b1aecc21222989b0e3d3
Gy74S <garysun0451@gmail.com>
2020-12-08 19:11:34 +1100

    Changed activity status and added printint of server info on ready up to console

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,14 @@
-const { prefix } = require('../config.json');
+const { prefix, activity } = require('../config.json');
 
 module.exports = async (client) => {
   try {
-    client.user.setActivity(`${prefix}jebeta`);
+    client.user.setActivity(`${prefix}${activity}`);
     console.log(`Active in ${client.guilds.cache.size} servers!`);
+    console.log(`${'Region'.padEnd(12)} | ${'Members'.padEnd(8)} | Name`);
+    console.log('-'.repeat(50));
+    client.guilds.cache.forEach(guild => {
+      console.log(`${guild.region.padEnd(12)} | ${guild.memberCount.toString().padEnd(8)} | ${guild.name}`);
+    });
     console.log(`${client.user.tag} is ready!`);
   } catch (err) {
     console.log(err);
