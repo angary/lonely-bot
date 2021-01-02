d55a6ac6d783e886995945f825d406e5bd69ec1b
Gy74S <garysun0451@gmail.com>
2021-01-02 20:52:27 +1100

    Remove printing details on startup to prevent error on null region
    Also remove profile picture

--- events/ready.js
+++ events/ready.js
@@ -4,11 +4,6 @@ module.exports = async (client) => {
   try {
     client.user.setActivity(`${prefix}${activity}`);
     console.log(`Active in ${client.guilds.cache.size} servers!`);
-    console.log(`${'Region'.padEnd(12)} | ${'Members'.padEnd(8)} | Name`);
-    console.log('-'.repeat(50));
-    client.guilds.cache.forEach(guild => {
-      console.log(`${guild.region.padEnd(12)} | ${guild.memberCount.toString().padEnd(8)} | ${guild.name}`);
-    });
     console.log(`${client.user.tag} is ready!`);
   } catch (err) {
     console.log(err);
