883d480b30d9243920a67a295fa3153266656ca9
Gary Sun <garysun0451@gmail.com>
2021-08-19 21:15:57 +1000

    Leave channel when there are no members in voice channel

---
+++ events/voiceStateUpdate.js
@@ -0,0 +1,21 @@
+
+module.exports = async (client, oldMember, newMember) => {
+  // Check the guild they are in
+  const guild = oldMember.guild;
+
+  // If the guild has music playing
+  const serverQueue = client.musicQueue.get(guild.id);
+  if (!serverQueue) {
+    console.log("Could not find serverQueue");
+    return;
+  }
+
+  console.log(serverQueue);
+
+  // Check if the voice channel is empty
+  if (serverQueue.voiceChannel.members.size === 1) {
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end();
+    return serverQueue.textChannel.send("Stopping music as all members have left the voice channel");
+  }
+};
